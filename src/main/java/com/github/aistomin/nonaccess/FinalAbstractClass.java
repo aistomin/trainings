1cdaec4d737bd62e29399253e94785f28f21246a
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-12 02:57:11 +0100

    Training #2. - abstract/final example.

---
+++ src/main/java/com/github/aistomin/nonaccess/FinalAbstractClass.java
@@ -0,0 +1,14 @@
+package com.github.aistomin.nonaccess;
+
+//public final abstract class FinalAbstractClass { final + abstract is impossible.
+
+final class FinalClass {
+}
+
+abstract class AbstractClass {
+
+    void concrete() {
+    }
+
+    abstract void abstracto();
+}

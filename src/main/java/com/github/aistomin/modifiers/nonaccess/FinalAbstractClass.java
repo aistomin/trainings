a11e2585cfc34d1d390f5ca4fce4ad6995de1a5c
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-12 03:10:27 +0100

    Training #2. - abstract method inside non-abstract class.

--- src/main/java/com/github/aistomin/modifiers/nonaccess/FinalAbstractClass.java
+++ src/main/java/com/github/aistomin/modifiers/nonaccess/FinalAbstractClass.java
@@ -3,6 +3,8 @@ package com.github.aistomin.modifiers.nonaccess;
 //public final abstract class FinalAbstractClass { final + abstract is impossible.
 
 final class FinalClass {
+
+//    abstract void abs(); impossible, class must be defined as abstract.
 }
 
 abstract class AbstractClass {

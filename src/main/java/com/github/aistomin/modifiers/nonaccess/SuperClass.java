da3da9419e333f695f11955f4004e2c3ca4ad954
aistomin <andrej.istomin.ikeen@gmail.com>
2016-03-18 21:02:04 +0100

    Training #4. synchronized methods

--- src/main/java/com/github/aistomin/modifiers/nonaccess/SuperClass.java
+++ src/main/java/com/github/aistomin/modifiers/nonaccess/SuperClass.java
@@ -1,5 +1,7 @@
 package com.github.aistomin.modifiers.nonaccess;
 
-public class SuperClass {
+public abstract class SuperClass {
     protected final void superMethod() {}
+//    abstract synchronized void sync(); // abstract + synchronized is impossible.
+
 }

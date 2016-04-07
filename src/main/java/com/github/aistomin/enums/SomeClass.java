d82a321b4da782d5ae8cb398cb69e891e964f81e
aistomin <andrej.istomin.ikeen@gmail.com>
2016-04-07 23:21:16 +0200

    Training #5. - enum with constructor.

--- src/main/java/com/github/aistomin/enums/SomeClass.java
+++ src/main/java/com/github/aistomin/enums/SomeClass.java
@@ -1,6 +1,9 @@
 package com.github.aistomin.enums;
 
 public class SomeClass {
+    public static void main(String[] args) {
+//        new Parametrised("test"); not allowed
+    }
 }
 
 enum Enum {
@@ -12,3 +15,18 @@ enum Enum {
 //    VALUE1,
 //    VALUE2
 //}
+
+enum Parametrised {
+    PARAM1("param1"),
+    PARAM2("param2");
+
+    private String param;
+
+    Parametrised(String param) {
+        this.param = param;
+    }
+
+    public String getParam() {
+        return param;
+    }
+}

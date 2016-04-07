b29a1882e3f1fa19b24e90bfa40f53d87a499393
aistomin <andrej.istomin.ikeen@gmail.com>
2016-04-07 23:14:57 +0200

    Training #5. - enums declaration.

---
+++ src/main/java/com/github/aistomin/enums/SomeClass.java
@@ -0,0 +1,14 @@
+package com.github.aistomin.enums;
+
+public class SomeClass {
+}
+
+enum Enum {
+    VALUE1,
+    VALUE2
+}
+
+//private enum Enum { // not allowed
+//    VALUE1,
+//    VALUE2
+//}
